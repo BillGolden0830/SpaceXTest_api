b90390322b6b671a1391755a8abac069f646b2d3
BillGolden0830 <william.golden@mobileconsultingsolutions.us>
2022-11-04 08:40:45 -0400

    Day 1 with OkHTTP

--- app/src/main/java/com/golden/spacextest/MainActivity.kt
+++ app/src/main/java/com/golden/spacextest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.golden.spacextest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.golden.spacextest.model.remote.SpacexNetwork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CoroutineScope(Job()).launch {
+            SpacexNetwork.spacexAPI.getCompanyInfo()
+        }
     }
 }
--- app/src/main/java/com/golden/spacextest/model/remote/SpacexNetwork.kt
+++ app/src/main/java/com/golden/spacextest/model/remote/SpacexNetwork.kt
@@ -1,18 +1,30 @@
 package com.golden.spacextest.model.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class SpacexNetwork {
+
+object SpacexNetwork {
     val spacexAPI: SpacexAPI by lazy{
         initRetrofit()
     }
 
-    private fun initRetrofit(){
-        return Retrofit.Builder()
-            .baseUrl()
-            .client()
-            .addConverterFactory(MoshiConverterFactory.create())
+
+    private fun initRetrofit(): SpacexAPI{
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
             .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+            .create(SpacexAPI::class.java)
     }
 }
